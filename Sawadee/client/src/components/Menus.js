b72062392a3205f37c90cc5a9820c3d018ca5bb2
Matt Berry <mberry41@gmail.com>
2018-01-06 11:49:58 -0700

    created menu list components

--- Sawadee/client/src/components/Menus.js
+++ Sawadee/client/src/components/Menus.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import MenuList from './MenuList';
+import MenuDetails from './MenuDetails';
 
 const Menus = () => {
-    return <div> Menus </div>
+    return(
+        <div>
+            <div>
+                <MenuList />
+            </div>
+            <div>
+                <MenuDetails />
+            </div>
+        </div>
+    )
 }
 
 export default Menus;
